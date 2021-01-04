a0ad9aeceba7bf504a4938851118f54b222fe563
Kapitan-Nemo <lasota020@gmail.com>
2021-01-04 23:27:12 +0100

    Changes for 404 file

--- wp-content/themes/dragonfly/404.php
+++ wp-content/themes/dragonfly/404.php
@@ -6,7 +6,7 @@
 
 get_header(); ?>
 
-<div id="content" class="mt-n6" role="main">
+<div id="content" role="main">
     <img class="vh-error-page vw-100" src="<?php echo get_template_directory_uri(); ?>/dist/images/Page_not_found_404.gif" alt="Page not found 404" title="Page not found 404"/>
     <p class="display-2 w-100 img-text-center text-center text-white">404! Page not found! </p>
 </div>
