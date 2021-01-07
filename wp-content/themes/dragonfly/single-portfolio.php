f4a285229726057a7b8753b681c1fc2d310ae5fa
Kapitan-Nemo <lasota020@gmail.com>
2021-01-07 23:08:33 +0100

    Single portfolio improve

--- wp-content/themes/dragonfly/single-portfolio.php
+++ wp-content/themes/dragonfly/single-portfolio.php
@@ -9,22 +9,28 @@
 
 
 <div id="post-content" class="container-fluid">
-<h1 class="position-relative display-1 text-center mt-6 font-weight-200"><?php the_title();?></h1>
+<h1 class="position-relative display-1 text-center mt-5 font-weight-200"><?php the_title();?></h1>
     <div class="row">
         <div class="col-md-6 col-12 text-justify order-2 order-md-1 my-auto">
                 <?php if (have_posts()) : while(have_posts()) : the_post();?>
-                <div class="d-inline d-block mb-2 mt-2 mt-md-0">
-                    <span class="mr-1">Category: </span>
-                <?php
-                    $terms = get_the_terms( $post->ID , 'categories' );
-                    if ( $terms != null ){
-                    foreach( $terms as $term ) {
-                    $term_link = get_term_link( $term, 'categories' );
-                    echo '<a class="badge badge-pill badge-dark px-3 py-2" href="' . $term_link . '">' . $term->name . '</a>';
-                    unset($term); } } ?>
-                <span class="ml-2 mr-1">Date:</span>
-                    <span class="badge badge-pill badge-dark px-3 py-2"><time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time></span>
-                </div>
+                <div class="mb-2 mt-2 mt-md-0 d-flex">
+                    <div class="my-3 text-center">
+                        <span class="mr-1">Category: </span>
+                        <?php
+                            $terms = get_the_terms( $post->ID , 'categories' );
+                            if ( $terms != null ){
+                            foreach( $terms as $term ) {
+                            $term_link = get_term_link( $term, 'categories' );
+                            echo '<a class="badge badge-pill badge-dark px-3 py-2" href="' . $term_link . '">' . $term->name . '</a>';
+                            unset($term); } } 
+                        ?>
+                    </div>
+                    <div class="my-3 text-center ml-md-3">
+                        <span class="ml-2 mr-1">Date:</span>
+                            <span class="badge badge-pill badge-dark px-3 py-2"><time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time></span>
+                        </div>
+                    </div>
+
                 <?php the_content();?>
             <?php endwhile; endif; ?>
         </div>
