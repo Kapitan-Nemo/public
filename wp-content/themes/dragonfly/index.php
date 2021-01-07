ed6c5edd8f22ccea245efb2c83c2ac97915fa516
Kapitan-Nemo <lasota020@gmail.com>
2021-01-07 21:38:49 +0100

    Fix for category page if have not posts

--- wp-content/themes/dragonfly/index.php
+++ wp-content/themes/dragonfly/index.php
@@ -1,18 +1,7 @@
 <?php get_header();?>
 <div id="posts-category" class="container py-5"> 
     <div id="gallery" class="row">
-        <p class="display-4 mb-5 col-12 text-center font-weight-200">Category: 
-            <?php
-                $terms = get_the_terms( $post->ID , 'categories' );
-                if ( $terms != null ) {
-                    foreach( $terms as $term ) {
-                        $term_link = get_term_link( $term, 'categories' );
-                        echo $term->name;
-                        unset($term); 
-                    } 
-                } 
-            ?>
-        </p>
+        
 
         <?php 
             $terms = wp_get_post_terms( $post->ID, 'categories'); 
@@ -37,6 +26,19 @@
             if ($query->have_posts()) :
             while ($query->have_posts()) : $query->the_post() 
         ?>
+
+        <p class="display-4 mb-5 col-12 text-center font-weight-200">Category: 
+            <?php
+                $terms = get_the_terms( $post->ID , 'categories' );
+                if ( $terms != null ) {
+                    foreach( $terms as $term ) {
+                        $term_link = get_term_link( $term, 'categories' );
+                        echo $term->name;
+                        unset($term); 
+                    } 
+                } 
+            ?>
+        </p>
 
        <div class="col-12 col-md-6 col-lg-4 parent">
           <a href="<?php the_permalink() ?>" >
@@ -72,7 +74,7 @@
         <?php endwhile ?>
         <?php else :?>
           <div class="col-12 text-center">
-            <p class="display-2 text-center mb-5">No posts to display!</p>
+            <p class="display-2 font-weight-200 text-center mb-5">No posts to display!</p>
             <img class="img-fluid rounded-lg" src="<?php echo get_template_directory_uri(); ?>/dist/images/No_posts_to_display.gif" alt="No posts to display" title="No posts to display"/>
           </div>   
         <?php endif; wp_reset_postdata();?>
